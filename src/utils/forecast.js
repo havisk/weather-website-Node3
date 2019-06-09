0984ab3cca40a0a8c0b686d483a5316507f9d4bd
Kool Havis <havisk@gmail.com>
2019-06-09 01:08:00 -0400

    edited forecast information

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -13,13 +13,10 @@ const forecast = (long, lat, callback) => {
         undefined,
         `${body.daily.data[0].summary} It is currently ${
           body.currently.temperature
-        } degrees outside. There is a ${
-          body.currently.precipProbability
-        } chance of rain. The high for the day is ${
+        } degrees outside. The high for today is ${
           body.daily.data[0].temperatureHigh
-        } degrees, The low for the day is ${
-          body.daily.data[0].temperatureLow
-        } degrees.`
+        } degrees with a low of ${body.daily.data[0].temperatureLow} degrees.
+        There is a ${body.currently.precipProbability}% chance of rain. `
       );
     }
   });
